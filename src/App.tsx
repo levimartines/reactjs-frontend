d8cb50c16028f9ca6810c6273335d30b0e07accb
Levi-Ferreira95 <levi_ferreira@external.mckinsey.com>
2022-04-09 10:31:13 -0300

    Styling improvements

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Login from './pages/Login/Login';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 const theme = createTheme();
 
@@ -16,6 +17,14 @@ function App() {
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline/>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login/>}/>
@@ -25,6 +34,7 @@ function App() {
             <Route path="*" element={<Navigate to="/dashboard"/>}/>
           </Routes>
         </BrowserRouter>
+        </Box>
       </Container>
     </ThemeProvider>
   );
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -62,32 +62,24 @@ export default function SignIn() {
 
   return (
     <>
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon/>
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlinedIcon/>
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
               margin="normal"
               required
               fullWidth
@@ -122,7 +114,6 @@ export default function SignIn() {
               </Grid>
             </Grid>
           </Box>
-        </Box>
       <Snackbar anchorOrigin={{ horizontal: 'center', vertical: 'top' }} open={loginFailedAlert}
                 autoHideDuration={4000} onClose={handleClose}>
         <Alert onClose={handleClose} severity="warning" sx={{ width: '100%' }}>
